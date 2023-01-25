757be3ee8c747428efb257f0f5eecf675a0504e0
Fred Drake <fred.drake@gmail.com>
2023-01-24 23:03:25 -0500

    Added hover documentation for coding

--- home/dot_config/lvim/config.lua
+++ home/dot_config/lvim/config.lua
@@ -21,6 +21,7 @@ lvim.builtin.which_key.mappings["o"] = {
 	d = { "<cmd>!sops -d -i %<CR><CR>", "Decrypt file" },
 	e = { "<cmd>!sops -e -i %<CR><CR>", "Encrypt file" },
 }
+lvim.builtin.which_key.mappings["K"] = { vim.lsp.buf.hover, "Hover Documentation" }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
