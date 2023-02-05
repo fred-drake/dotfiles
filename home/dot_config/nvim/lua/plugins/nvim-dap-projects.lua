f8c6d93967d9df67c5e7e90d5de25e053ffaedfc
Fred Drake <fred.drake@gmail.com>
2023-02-05 12:09:31 -0500

    Fixed plugin spec

--- home/dot_config/nvim/lua/plugins/nvim-dap-projects.lua
+++ home/dot_config/nvim/lua/plugins/nvim-dap-projects.lua
@@ -1,8 +1,6 @@
 return {
   "ldelossa/nvim-dap-projects",
-  dependencies = {
-    config = function()
-      require("nvim-dap-projects").search_project_config()
-    end,
-  },
+  config = function()
+    require("nvim-dap-projects").search_project_config()
+  end,
 }
