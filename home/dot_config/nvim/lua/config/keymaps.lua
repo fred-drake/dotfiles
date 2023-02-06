68befdd8ca6f6689de42f4329aecb4997d20d4de
Fred Drake <fred.drake@gmail.com>
2023-02-06 00:01:18 -0500

    Added debugging routines

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jk", "<cmd>noh<cr><esc>")
+vim.keymap.set("i", "jk", "<cmd>noh<cr><esc>", { desc = "Normal mode and clear highlight" })
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>", { desc = "Debug - Continue" })
+vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", { desc = "Debug - Step Over" })
+vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", { desc = "Debug - Step Into" })
+vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>", { desc = "Debug - Step Out" })
+vim.keymap.set("n", "<leader>cb", ":lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debug - Set Breakpoint" })
+vim.keymap.set(
+  "n",
+  "<leader>cB",
+  ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition:'))<CR>",
+  { desc = "Debug - Set Conditional Breakpoint" }
+)
+vim.keymap.set("n", "<leader>cR", ":lua require'dap'.repl.open()<CR>", { desc = "Debug - Repl Open" })
