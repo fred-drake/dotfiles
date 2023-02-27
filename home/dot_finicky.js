79763f879222c01ab63b59577092d4bbe75aa7c2
Fred Drake <fred.drake@gmail.com>
2023-02-27 16:14:08 -0500

    Added lessonly to chrome

--- home/dot_finicky.js
+++ home/dot_finicky.js
@@ -2,80 +2,84 @@ module.exports = {
   handlers: [
     {
       match: finicky.matchDomains("open.spotify.com"),
-      browser: "Spotify"
+      browser: "Spotify",
     },
     {
       match: /theoremco.slack.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /miro.com/,
-      browser: "Miro"
+      browser: "Miro",
     },
     {
       match: /meet.google.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /docs.google.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /calendar.google.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /notion.so\/theoremone/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /theoremone.okta.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /theoremone.namely.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /theoremco.atlassian.net/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /abacus.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /officevibe.com/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /hire.lever.co/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /linktr.ee\/TheoremOneTA/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
+    },
+    {
+      match: /theoremone.*?/,
+      browser: "Google Chrome",
     },
     {
       match: /apple.com/,
-      browser: "Safari"
+      browser: "Safari",
     },
     {
       match: /a4502581.slack.com/,
-      browser: "Safari"
+      browser: "Safari",
     },
     {
       match: /apple.enterprise.slack.com/,
-      browser: "Safari"
+      browser: "Safari",
     },
     {
       match: /apple.slack.com/,
-      browser: "Safari"
+      browser: "Safari",
     },
     {
       match: /apple.box.com/,
-      browser: "Safari"
+      browser: "Safari",
     },
   ],
-  defaultBrowser: "Brave Browser"
-}
+  defaultBrowser: "Brave Browser",
+};
