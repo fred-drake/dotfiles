5726975890f40d9502baea1d894cf215b253ef65
Fred Drake <fred.drake@gmail.com>
2023-05-04 22:13:53 -0400

    Added wireguard to VPN output

--- home/dot_config/sketchybar/plugins/vpn/executable_vpn.sh
+++ home/dot_config/sketchybar/plugins/vpn/executable_vpn.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+WG=$(wg show interfaces)
 VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/' | awk -v d=" / " '{s=(NR==1?s:s d)$0}END{print s}')
 
 if [[ $VPN != "" ]]; then
-  sketchybar -m --set $NAME label="$VPN" label.drawing="on" icon.drawing="on"
+	if [[ $WG != "" ]]; then
+		VPN="${VPN} / Wireguard"
+	fi
 else
-  sketchybar -m --set $NAME label.drawing="off" icon.drawing="off"
+	if [[ $WG != "" ]]; then
+		VPN="Wireguard"
+	fi
 fi
 
+if [[ $VPN != "" ]]; then
+	sketchybar -m --set $NAME label="$VPN" label.drawing="on" icon.drawing="on"
+else
+	sketchybar -m --set $NAME label.drawing="off" icon.drawing="off"
+fi
