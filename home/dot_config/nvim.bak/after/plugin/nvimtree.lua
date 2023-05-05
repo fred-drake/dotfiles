66ed4de14dc8c909c4ba705a6d741113e62c1454
Fred Drake <fred.drake@gmail.com>
2023-05-04 22:04:44 -0400

    Cleaned up nvim and lvim configurations

--- home/dot_config/nvim.bak/after/plugin/nvimtree.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-tree").setup({
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false,
-            },
-        },
-    },
-})
--- home/dot_config/nvim.bak/lua/user/init.lua
+++
@@ -1,6 +0,0 @@
-require("user.packer")
-require("user.remap")
-require("user.set")
-
-vim.o.background = "dark" 
-vim.cmd([[colorscheme gruvbox]])
