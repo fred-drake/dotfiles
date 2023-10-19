b7fe28c2eccc2aa46f7eae4fdca2e8824e9f6e84
Fred Drake <fred.drake@gmail.com>
2023-10-19 18:23:56 -0400

    Changed browser preference to Thorium

--- home/dot_finicky.js
+++ home/dot_finicky.js
@@ -6,63 +6,63 @@ module.exports = {
     },
     {
       match: /theoremco.slack.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /meet.google.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /docs.google.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /calendar.google.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /notion.so\/theoremone/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /theoremone.okta.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /theoremone.namely.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /theoremco.atlassian.net/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /abacus.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /officevibe.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /hire.lever.co/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /linktr.ee\/TheoremOneTA/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /theoremone.*?/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /miro.com/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /ro.am/,
-      browser: "Google Chrome",
+      browser: "Thorium",
     },
     {
       match: /apple.com/,
