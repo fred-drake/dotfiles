8e6d68144d0aa7211bcab89cbaaf334726f28375
Fred Drake <fred.drake@gmail.com>
2023-12-09 23:56:05 -0500

    Removed sketchybar / yabai / skhd

--- home/bin/executable_toggle_audio.sh
+++ home/bin/executable_toggle_audio.sh
@@ -3,11 +3,9 @@
 CURRENT_OUTPUT=$(SwitchAudioSource -t output -c)
 
 if [[ "$CURRENT_OUTPUT" = "External Headphones" ]]; then
-  SwitchAudioSource -t output -s "PRO X Wireless Gaming Headset"
-  SwitchAudioSource -t input -s "PRO X Wireless Gaming Headset"
+	SwitchAudioSource -t output -s "PRO X Wireless Gaming Headset"
+	SwitchAudioSource -t input -s "PRO X Wireless Gaming Headset"
 else
-  SwitchAudioSource -t output -s "External Headphones"
-  SwitchAudioSource -t input -s "NexiGo N60 FHD Webcam"
+	SwitchAudioSource -t output -s "External Headphones"
+	SwitchAudioSource -t input -s "NexiGo N60 FHD Webcam"
 fi
-
-sketchybar --trigger audio_update
